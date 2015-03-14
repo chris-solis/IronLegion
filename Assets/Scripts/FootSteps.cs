7f52ceea9112237611f48fc5047ea117f13917ca
Chris Solis <solisalp@gmail.com>
2015-03-14 11:53:16 -0600

    Adding Sounds
    Added music to the scenes and added sounds to mega movement.

---
+++ Assets/Scripts/FootSteps.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootSteps : MonoBehaviour {
+	
+
+	public AudioClip stepSound;
+	public AudioClip jumpSound;
+
+	private AudioSource source;
+	private float volLowRange = .5f;
+	private float volHighRange = 1.5f;
+	
+	// Use this for initialization
+	void Awake	 () {
+		
+		source = GetComponent <AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Walk")) 
+		{
+			source.loop = true;
+			float vol = Random.Range (volLowRange, volHighRange);
+			source.PlayOneShot(stepSound, vol);
+
+			
+		}
+		if (Input.GetButtonDown("Jump"))
+		{
+			float vol = Random.Range (volLowRange, volHighRange);
+			source.PlayOneShot(jumpSound, vol);
+		}
+	}
+}
---
+++ Assets/Scripts/RoboShot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoboShot : MonoBehaviour {
+
+	public AudioClip shootSound;
+
+	private AudioSource source;
+	private float volLowRange = .5f;
+	private float volHighRange = 1.5f;
+	
+	// Use this for initialization
+	void Awake	 () {
+		
+		source = GetComponent <AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			float vol = Random.Range (volLowRange, volHighRange);
+			source.PlayOneShot(shootSound, vol);
+
+			
+		}
+	}
+}
+
